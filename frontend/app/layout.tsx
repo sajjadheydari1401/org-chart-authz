92b5c5742741c31b1a2bfdb94ef01b689ef0f173
Sajjad <sajjadheydari1401@gmail.com>
2026-09-25 17:38:15 +0330

    feat(frontend): make the app dir rtl

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -20,6 +20,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
+      dir="rtl"
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
